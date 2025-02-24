879562b5bc0d4925d83b2cf3ba57f5f0867430ee
g-irakora <g.irakora@alustudent.com>
2025-02-24 13:40:34 +0200

    Added my Scripts

---
+++ python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str= print f"{str[20:47] with Python}
+print(str)
