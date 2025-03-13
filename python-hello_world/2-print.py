f51a6aab2e8b074ae2571196eba27f254be8f11d
g-irakora <g.irakora@alustudent.com>
2025-03-13 11:44:02 +0200

    Made changes

--- python-hello_world/2-print.py
+++ python-hello_world/2-print.py
@@ -1,2 +1,3 @@
 #!/usr/bin/python3
-print ("Programming is like building a multilingual puzzle")
+print (""Programing is like building a multilingual puzzle"
+)
---
+++ python-hello_world/cars.py
@@ -0,0 +1,5 @@
+car_brands=['Toyota','Mercedes','Audi','Volkswagen']
+counter= 1
+for car_brand in car_brands:
+    print(f"Car Number {counter} is {car_brand}")
+    counter +=1
